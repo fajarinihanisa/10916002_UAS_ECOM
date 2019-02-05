fa4915f83682798e4894cf704ece23f32d882a3f
fajarinihanisa <fajarinihanisa@gmail.com>
2019-02-05 23:47:44 +0700

    First commit

---
+++ database/db_laravel_siswa (1).sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2019 at 04:15 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_laravel_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2019_02_02_075523_create_table_kelas', 1),
+(2, '2019_02_02_142336_create_table_siswa', 2),
+(3, '2019_02_02_163046_create_table_user', 3),
+(4, '2019_02_02_172942_add_foto_table_siswa', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_kelas`
+--
+
+CREATE TABLE `t_kelas` (
+  `id_kelas` int(10) UNSIGNED NOT NULL,
+  `nama_kelas` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `t_kelas`
+--
+
+INSERT INTO `t_kelas` (`id_kelas`, `nama_kelas`, `jurusan`, `created_at`, `updated_at`) VALUES
+(2, 'X-RPL2', 'Rekayasa Perangkat Lunak', NULL, '2019-02-05 07:27:21'),
+(4, 'XII-RPL1', 'Rekaya Perangkat Lunak', '2019-02-02 02:34:14', '2019-02-02 02:42:49'),
+(5, 'XII-TKJ1', 'Teknik Komputer dan Jaringan', '2019-02-02 02:42:33', '2019-02-02 02:42:33'),
+(6, 'X-MM1', 'Multimedia', '2019-02-02 03:12:06', '2019-02-02 07:20:59'),
+(11, 'XII-MM2', 'Multimedia', '2019-02-05 07:28:35', '2019-02-05 07:28:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_login`
+--
+
+CREATE TABLE `t_login` (
+  `id_login` int(10) UNSIGNED NOT NULL,
+  `nama_user` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `t_login`
+--
+
+INSERT INTO `t_login` (`id_login`, `nama_user`, `username`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin', '$2y$10$5NYmsqHY0WkDOrtKgrujJOB7SGl/kbUlmxcH4OfJeFiq7jIFMGDPm', 'zedxK10OyBQKXMObTTyjF35O5kth4S3c0GjxILy3ywIprxIDttfFlqGKWSvw', '2019-02-02 10:00:22', '2019-02-02 10:00:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_siswa`
+--
+
+CREATE TABLE `t_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama_lengkap` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kelamin` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `foto` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `t_siswa`
+--
+
+INSERT INTO `t_siswa` (`nis`, `nama_lengkap`, `jenis_kelamin`, `alamat`, `no_telp`, `id_kelas`, `created_at`, `updated_at`, `foto`) VALUES
+(46574232, 'Alwi Asegaf', 'L', 'Jl. Banteng Barat,No. 102,  Bandung', '085697654321', 5, '2019-02-05 07:30:25', '2019-02-05 07:30:25', '46574232.jpg'),
+(56789021, 'Zahra Faujiah', 'P', 'Jl. Kencana Ungu No.61, Bandung', '0879654321', 6, '2019-02-05 08:11:00', '2019-02-05 08:11:00', '56789021.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_kelas`
+--
+ALTER TABLE `t_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `t_login`
+--
+ALTER TABLE `t_login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `t_siswa`
+--
+ALTER TABLE `t_siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `t_kelas`
+--
+ALTER TABLE `t_kelas`
+  MODIFY `id_kelas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `t_login`
+--
+ALTER TABLE `t_login`
+  MODIFY `id_login` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,6 @@
+# 10916002_UAS_ECOM
+Dalam membangun project ini, 
+pertama, persiapkan template AdminLTE 2.3.11 yang bisa didapatkan dari alamat https://codeload.github.com/almasaeed2010/AdminLTE/zip/v2.3.11. 
+Kedua, membaca data dari database menggunakan Laravel dan Eloquent. Eloquent sendiri merupakan fitur dari Laravel yang memungkinkan kita memanggil data dari database dalam bentuk Entity Object, tanpa syntax MySQL sama sekali. Untuk membuat database MySQL nya pun akan digunakan fitur migration dari Laravel. Migration ini membantu web artisan dalam mendefinisikan table (DDL) dalam bentuk sintaks pemrograman PHP, tutorialnya bisa di lihat pada link ini https://drive.google.com/file/d/1WpHAgdv4zVrgA-nV1u64Mbl31C65LyVC/view?usp=sharing .
+Ketiga, tutorial Authentikasi User Kita akan menggunakan fitur bawaan Laravel dan sedikit modifikasi sistemnya tutorialnya bisa kita lihat pada link ini https://drive.google.com/file/d/1wLjs3QIaYI3o9mIikLBJOluA_7QTm6rQ/view?usp=sharing. 
+Keempat, mengupload file di laravel tutorialnya bisa dilihat pada link ini https://drive.google.com/file/d/1-qb34ta4QJFzmekmiUAK84CzW6Cy7IXR/view?usp=sharing
